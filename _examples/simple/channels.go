ea550b1f0a3819fc8b3819009abeb1f1682a198c
Joey Lee <joey@operata.com>
2020-07-29 18:19:21 +1000

    Combine mono to new audio segment

--- _examples/simple/channels.go
+++ _examples/simple/channels.go
@@ -13,3 +13,20 @@ func splitToMono() {
 	monoSegments, _ := segment.SplitToMono()
 	fmt.Println(monoSegments)
 }
+
+func combineMono() {
+	lFilePath := path.Join(dataDirectory(), "testing-stereo-left-channel.wav")
+	lSegment, _ := godub.NewLoader().Load(lFilePath)
+
+	rFilePath := path.Join(dataDirectory(), "testing-stereo-right-channel.wav")
+	rSegment, _ := godub.NewLoader().Load(rFilePath)
+
+	combined, _ := godub.NewAudioSegmentFromMono(lSegment, rSegment)
+	fmt.Println(combined)
+
+	// Export to file
+	toFilePath := path.Join(tmpDataDirectory(), "combined-testing-stereo.wav")
+	godub.NewExporter(toFilePath).
+		WithDstFormat("wav").
+		Export(combined)
+}
